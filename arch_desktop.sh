ce2a41b10882fbf7423b05e499c75f70d476ddde
aleks <aleksandrs.stier@uni-bielefeld.de>
2018-11-26 10:41:20 +0100

    Add xautolock-package

--- arch_desktop.sh
+++ arch_desktop.sh
@@ -6,9 +6,7 @@ sudo pacman -S reflector
 sudo reflector -c Germany -p https -l10 --sort rate --save /etc/pacman.d/mirrorlist
 
 # Packages
-sudo pacman -S arc-gtk-theme file-roller firefox gedit git gvfs htop network-manager-applet noto-fonts pavucontrol pulseaudio pycharm-community-edition screenfetch slock ttf-dejavu thunar-archive-plugin ufw xfce4 xfce4-notifyd xfce4-pulseaudio-plugin xfce4-screenshooter xfce4-whiskermenu-plugin xorg-server # nvidia
-# TODO test blanking with and without nvidia
-# TODO test acpid on laptop
+sudo pacman -S arc-gtk-theme file-roller firefox gedit git gvfs htop network-manager-applet noto-fonts pavucontrol pulseaudio pycharm-community-edition screenfetch slock ttf-dejavu thunar-archive-plugin ufw xautolock xfce4 xfce4-notifyd xfce4-pulseaudio-plugin xfce4-screenshooter xfce4-whiskermenu-plugin xorg-server # nvidia
 sudo pacman -Rs xfwm4-themes
 sudo systemctl stop dhcpcd
 sudo systemctl enable --now NetworkManager.service systemd-timesyncd.service ufw.service
