c7abbb20b87f006342710a36f8a51798770491e4
aleks <aleksandrs.stier@uni-bielefeld.de>
2018-11-27 01:28:13 +0100

    Remove xfce4-notifyd and add TODOs

--- arch_desktop.sh
+++ arch_desktop.sh
@@ -6,7 +6,9 @@ sudo pacman -S reflector
 sudo reflector --info -p https -l256 -f32 --score 8 rate --save /etc/pacman.d/mirrorlist
 
 # Packages
-sudo pacman -S arc-gtk-theme file-roller firefox gedit git gvfs htop network-manager-applet noto-fonts pavucontrol pulseaudio pycharm-community-edition slock ttf-dejavu thunar-archive-plugin ufw xautolock xfce4 xfce4-notifyd xfce4-pulseaudio-plugin xfce4-screenshooter xfce4-whiskermenu-plugin xorg-server # nvidia
+sudo pacman -S arc-gtk-theme file-roller firefox gedit git gvfs htop network-manager-applet noto-fonts pavucontrol pulseaudio pycharm-community-edition slock ttf-dejavu thunar-archive-plugin ufw xautolock xfce4 xfce4-pulseaudio-plugin xfce4-screenshooter xfce4-whiskermenu-plugin xorg-server # nvidia
+# TODO try systemd-networkd + wpa_cli
+# TODO try systemd-powermanager
 sudo pacman -Rns netctl vi xfwm4-themes
 sudo systemctl stop dhcpcd
 sudo systemctl enable --now NetworkManager.service systemd-timesyncd.service ufw.service
