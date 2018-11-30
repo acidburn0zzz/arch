211830b2a0bf2aa8b76d49d1a03332cd613d93ac
aleks <aleksandrs.stier@uni-bielefeld.de>
2018-11-30 11:58:53 +0100

    Change comments

--- arch_base.sh
+++ arch_base.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# TODO SCRIPT
+
 loadkeys de
 timedatectl set-ntp 1
 
--- arch_desktop.sh
+++ arch_desktop.sh
@@ -9,32 +9,27 @@ sudo reflector -p https -f16 --score 8 --sort rate --save /etc/pacman.d/mirrorli
 sudo pacman -S arc-gtk-theme cinnamon firefox git gnome-terminal nemo-fileroller ufw xorg-server xorg-xinit # eog evince htop nvidia pycharm-community-edition
 sudo systemctl enable NetworkManager systemd-timesyncd.service ufw.service
 
-# AUR
-# TODO write custom scripts
+# AUR TODO write custom scripts / AUR helper
 mkdir AUR && cd AUR
 git clone https://aur.archlinux.org/flat-remix-git.git
 cd flat-remix-git && makepkg -Ccirs && cd ..
 git clone https://aur.archlinux.org/dropbox.git
 cd dropbox && makepkg -Ccirs
 
-# Miniconda
-cd && curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh
-rm Miniconda3-latest-Linux-x86_64.sh .bashrc-miniconda3.bak
-. .bashrc && conda update --all
-
-# Configuration
-mkdir .config Projects && cd Projects
+# Dotfiles
+cd && mkdir .config Projects && cd Projects
 git clone https://github.com/astier/arch-installer.git
 git clone https://github.com/astier/dotfiles.git
 cd dotfiles && sh install.sh
 
+# Miniconda
+cd && curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+sh Miniconda3-latest-Linux-x86_64.sh
+rm Miniconda3-latest-Linux-x86_64.sh
+conda update --all
+
 reboot
 
-# Maintenance
-# TODO health-check-script
-# kernel, systemd, journal, xorg
-# systemctl --all --failed
-# journalctl -p3 -xb / -xe
-# sudo pacman -Rns$(pacman -Qtdg)
-# TODO silent boot
+# TODO health-check
+# TODO silent-boot
+# TODO system-cleaner
