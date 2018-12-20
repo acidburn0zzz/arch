7ad8dd7bcbe8268e738e51f9ab6250d2fc556a57
aleks <aleksandrs.stier@uni-bielefeld.de>
2018-12-20 02:30:37 +0100

    Install vscode and remove scripts

--- arch_desktop.sh
+++ arch_desktop.sh
@@ -1,30 +1,46 @@
 #!/bin/bash
 
+# TODO set x-keymap
+
 # Mirrorlist
 sudo systemctl start dhcpcd
 sudo pacman -S reflector
 sudo reflector -p https -l64 -f16 --score 8 --sort rate --save /etc/pacman.d/mirrorlist
 
 # Packages
-sudo pacman -S arc-gtk-theme cinnamon eog firefox git gnome-screenshot gnome-terminal htop nemo-fileroller numlockx ufw xorg-server xorg-xinit # nvidia
+sudo pacman -S arc-gtk-theme cinnamon eog firefox git gnome-screenshot gnome-terminal htop nemo-fileroller neovim numlockx ufw xorg-server xorg-xinit # nvidia
 sudo systemctl enable NetworkManager systemd-timesyncd.service ufw.service
+sudo pacman -Rns nano netctl vi
 
 # AUR
 git clone https://aur.archlinux.org/yay.git
 cd yay && makepkg -Ccirs && cd .. && rm -rf yay
 yay -S dropbox flat-remix-git
 
-# Dotfiles
-mkdir ~/Projects && cd ~/Projects
-git clone https://github.com/astier/dotfiles.git
-cd dotfiles && sh install.sh
-
-# Scripts
-cd ~/Projects
-git clone https://github.com/astier/scripts.git
-cd scripts && sh install.sh
+#VSC
+sudo pacman -S ctags npm
+sudo npm --unsafe-perm i -g bash-language-server
+yay -S dropbox flat-remix-git visual-studio-code-bin
+code --install-extension davidanson.vscode-markdownlint
+code --install-extension equinusocio.vsc-material-theme
+code --install-extension formulahendry.code-runner
+code --install-extension mads-hartmann.bash-ide-vscode
+code --install-extension ms-python.python
+code --install-extension pkief.material-icon-theme
+code --install-extension rogalmic.bash-debug
+code --install-extension shakram02.bash-beautify
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension visualstudioexptteam.vscodeintellicode
 
 # Miniconda
 curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 sh Miniconda3-latest-Linux-x86_64.sh
 rm Miniconda3-latest-Linux-x86_64.sh
+conda update --all
+conda create -n isy autopep8 keras matplotlib pandas pylint rope scikit-learn
+conda clean -ay
+
+# Dotfiles
+mkdir ~/Projects && cd ~/Projects
+git clone https://github.com/astier/dotfiles.git
+cd dotfiles && sh install.sh
