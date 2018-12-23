9df85efc0824725407de0d58e221be78d4ba3568
aleks <aleksandrs.stier@uni-bielefeld.de>
2018-12-23 11:15:42 +0100

    Add vscode-plugins

--- arch_desktop.sh
+++ arch_desktop.sh
@@ -8,8 +8,8 @@ sudo pacman -S reflector
 sudo reflector -p https -l64 -f16 --score 8 --sort rate --save /etc/pacman.d/mirrorlist
 
 # Packages
-sudo pacman -S arc-gtk-theme cinnamon eog firefox git gnome-screenshot gnome-terminal htop nemo-fileroller neovim numlockx ufw xorg-server xorg-xinit # nvidia
-sudo systemctl enable NetworkManager systemd-timesyncd.service ufw.service
+sudo pacman -S arc-gtk-theme cinnamon eog firefox git gnome-screenshot gnome-terminal htop nemo-fileroller neovim numlockx texlive-core ttf-baekmuk ufw xorg-server xorg-xinit # nvidia
+sudo systemctl enable NetworkManager.service systemd-timesyncd.service ufw.service
 sudo pacman -Rns nano netctl vi
 
 # AUR
@@ -18,13 +18,15 @@ cd yay && makepkg -Ccirs && cd .. && rm -rf yay
 yay -S dropbox flat-remix-git
 
 #VSC
-sudo pacman -S ctags npm
+sudo pacman -S ctags npm pandoc xdg-utils
 sudo npm --unsafe-perm i -g bash-language-server
 yay -S visual-studio-code-bin
 code --install-extension davidanson.vscode-markdownlint
+code --install-extension dougfinke.vscode-pandoc
 code --install-extension dunstontc.viml
 code --install-extension equinusocio.vsc-material-theme
 code --install-extension formulahendry.code-runner
+code --install-extension james-yu.latex-workshop
 code --install-extension mads-hartmann.bash-ide-vscode
 code --install-extension ms-python.python
 code --install-extension pkief.material-icon-theme
@@ -32,6 +34,7 @@ code --install-extension rogalmic.bash-debug
 code --install-extension shakram02.bash-beautify
 code --install-extension streetsidesoftware.code-spell-checker
 code --install-extension visualstudioexptteam.vscodeintellicode
+code --install-extension yzhang.markdown-all-in-one
 
 # Miniconda
 curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
