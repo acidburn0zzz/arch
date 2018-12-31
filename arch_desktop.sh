e584aa8fc21ab8277b95461cd12e4e982afc40eb
astier <aleksandrs.stier@uni-bielefeld.de>
2018-12-31 17:28:29 +0100

    Replace vscode with neovim

--- arch_desktop.sh
+++ arch_desktop.sh
@@ -16,36 +16,20 @@ sudo pacman -S arc-gtk-theme cinnamon feh firefox git gnome-screenshot gnome-ter
 sudo systemctl enable NetworkManager.service systemd-timesyncd.service ufw.service
 sudo pacman -Rns nano netctl vi
 
+# Neovim
+sudo pacman -S neovim pandoc powerline powerline-fonts texlive-core 
+
 # AUR
 git clone https://aur.archlinux.org/yay.git
 cd yay && makepkg -Ccirs && cd .. && rm -rf yay
 yay -S dropbox flat-remix-git
-
-# VSC
-sudo pacman -S bash-language-server ctags pandoc texlive-core xdg-utils
-yay -S visual-studio-code-bin
-code --install-extension davidanson.vscode-markdownlint
-code --install-extension dougfinke.vscode-pandoc
-code --install-extension dunstontc.viml
-code --install-extension equinusocio.vsc-material-theme
-code --install-extension formulahendry.code-runner
-code --install-extension james-yu.latex-workshop
-code --install-extension mads-hartmann.bash-ide-vscode
-code --install-extension ms-python.python
-code --install-extension pkief.material-icon-theme
-code --install-extension rogalmic.bash-debug
-code --install-extension rpinski.shebang-snippets
-code --install-extension shakram02.bash-beautify
-code --install-extension streetsidesoftware.code-spell-checker
-code --install-extension visualstudioexptteam.vscodeintellicode
-code --install-extension yzhang.markdown-all-in-one
 
 # Miniconda
 curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 sh Miniconda3-latest-Linux-x86_64.sh
 rm Miniconda3-latest-Linux-x86_64.sh
 conda update --all
-conda create -n isy autopep8 keras matplotlib pandas pylint rope scikit-learn
+conda create -n isy keras matplotlib scikit-learn
 conda clean -ay
 
 # Dotfiles
