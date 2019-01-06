55e727d62de5802d12bb8686fd433441fc5049be
astier <aleksandrs.stier@uni-bielefeld.de>
2019-01-06 12:05:41 +0100

    Replace feh with eog

--- arch_desktop.sh
+++ arch_desktop.sh
@@ -12,7 +12,7 @@ sudo pacman -S reflector
 sudo reflector -p https -l64 -f16 --score 8 --sort rate --save /etc/pacman.d/mirrorlist
 
 # Packages
-sudo pacman -S arc-gtk-theme biber cinnamon feh firefox git gnome-screenshot gnome-terminal htop nemo-fileroller neofetch numlockx pandoc texlive-bibtexextra texlive-core ttd-dejavu ttf-baekmuk ufw xorg-server xorg-xinit # nvidia
+sudo pacman -S arc-gtk-theme biber cinnamon eog firefox git gnome-screenshot gnome-terminal htop nemo-fileroller neofetch numlockx pandoc texlive-bibtexextra texlive-core ttd-dejavu ttf-baekmuk ufw xorg-server xorg-xinit # nvidia
 sudo systemctl enable NetworkManager.service systemd-timesyncd.service ufw.service
 
 # Neovim
