d89c9804552a9eb591700620cec9114048e79b6d
astier <aleksandrs.stier@uni-bielefeld.de>
2019-01-06 23:49:41 +0100

    Add neovim

--- arch_desktop.sh
+++ arch_desktop.sh
@@ -12,9 +12,8 @@ sudo pacman -S reflector
 sudo reflector -p https -l64 -f16 --score 8 --sort rate --save /etc/pacman.d/mirrorlist
 
 # Packages
-sudo pacman -S arc-gtk-theme biber cinnamon eog firefox gedit git gnome-screenshot gnome-terminal htop nemo-fileroller numlockx pandoc texlive-bibtexextra texlive-core ttd-dejavu ttf-baekmuk ufw xorg-server xorg-xinit # nvidia
+sudo pacman -S arc-gtk-theme biber cinnamon eog firefox git gnome-screenshot gnome-terminal htop nemo-fileroller numlockx pandoc texlive-bibtexextra texlive-core ttd-dejavu ttf-baekmuk ufw xorg-server xorg-xinit # nvidia
 sudo systemctl enable NetworkManager.service systemd-timesyncd.service ufw.service
-sudo pacman -Rns netctl nano
 
 # AUR
 git clone https://aur.archlinux.org/yay.git
@@ -29,10 +28,12 @@ conda update --all
 conda create -n isy autopep8 keras matplotlib pandas pylint rope scikit-learn
 conda clean -ay
 
+# VSCode
 sudo pacman -S bash-language-server ctags shellcheck xdg-utils
 yay -S visual-studio-code-bin
 code --install-extension davidanson.vscode-markdownlint
 code --install-extension dougfinke.vscode-pandoc
+code --install-extension dunstontc.viml
 code --install-extension equinusocio.vsc-material-theme
 code --install-extension formulahendry.code-runner
 code --install-extension james-yu.latex-workshop
@@ -46,6 +47,11 @@ code --install-extension streetsidesoftware.code-spell-checker
 code --install-extension timonwong.shellcheck
 code --install-extension visualstudioexptteam.vscodeintellicode
 code --install-extension yzhang.markdown-all-in-one
+
+# Neovim
+sudo pacman -S neovim powerline powerline-fonts
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+sudo pacman -Rns nano vi
 
 # Dotfiles
 mkdir ~/Projects && cd ~/Projects
