6146e4d08b6b414aef6b552f28178e79b894b57f
astier <aleksandrs.stier@uni-bielefeld.de>
2019-01-16 01:29:01 +0100

    Add back neovim because vi sucks

--- arch_desktop.sh
+++ arch_desktop.sh
@@ -16,7 +16,6 @@ sudo reflector -p https -l64 -f16 --score 8 --sort rate --save /etc/pacman.d/mir
 # Packages
 sudo pacman -S arc-gtk-theme biber cinnamon eog evince firefox git gnome-screenshot gnome-terminal htop nemo-fileroller noto-fonts-emoji numlockx pandoc texlive-bibtexextra texlive-core ttd-dejavu ttf-baekmuk ufw xorg-server xorg-xinit # nvidia
 sudo systemctl enable NetworkManager.service systemd-timesyncd.service ufw.service
-sudo pacman -Rns nano netctl
 
 # AUR
 git clone https://aur.archlinux.org/yay.git
@@ -29,6 +28,11 @@ sh Miniconda3-latest-Linux-x86_64.sh
 rm Miniconda3-latest-Linux-x86_64.sh
 conda create -n isy keras matplotlib pandas scikit-learn
 
+# Neovim
+sudo pacman -S neovim powerline-fonts
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+sudo pacman -Rns nano vi
+
 # VSCode
 yay -S visual-studio-code-bin
 sudo pacman -S bash-language-server ctags shellcheck xdg-utils
@@ -37,6 +41,7 @@ conda install autopep8 pylint
 code --install-extension arcticicestudio.nord-visual-studio-code
 code --install-extension davidanson.vscode-markdownlint
 code --install-extension dougfinke.vscode-pandoc
+code --install-extension dunstontc.viml
 code --install-extension equinusocio.vsc-material-theme
 code --install-extension formulahendry.code-runner
 code --install-extension grapecity.gc-excelviewer
