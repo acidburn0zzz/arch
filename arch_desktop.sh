8c66f591f37f3c807df5966769556d10970d3d69
astier <aleksandrs.stier@uni-bielefeld.de>
2019-02-17 18:57:34 +0100

    Replace vscode with neovim

--- arch_desktop.sh
+++ arch_desktop.sh
@@ -12,7 +12,7 @@ sudo pacman -S reflector
 sudo reflector -p https -f32 -l16 --score 8 --sort rate --save /etc/pacman.d/mirrorlist
 
 # Packages
-sudo pacman -S arc-gtk-theme biber cinnamon eog firefox git gnome-screenshot gnome-terminal htop libreoffice-fresh nemo-fileroller neovim numlockx texlive-bibtexextra texlive-core ttf-baekmuk ttf-dejavu ufw xorg-server xorg-xinit # nvidia
+sudo pacman -S arc-gtk-theme biber cinnamon eog evince firefox git gnome-screenshot gnome-terminal htop libreoffice-fresh nemo-fileroller neovim noto-fonts-cjk numlockx shellcheck texlive-bibtexextra texlive-core ttf-dejavu ufw xorg-server xorg-xinit # nvidia
 sudo systemctl enable NetworkManager.service systemd-timesyncd.service ufw.service
 pacman -Rns nano netctl vi
 
@@ -28,27 +28,6 @@ rm Miniconda3-latest-Linux-x86_64.sh .bashrc-miniconda3.bak
 . .bashrc && conda update --all
 conda create -n isy jupyter keras matplotlib pandas scikit-learn
 
-# VSCode
-yay -S ttf-twemoji-color visual-studio-code-bin
-sudo pacman -S ctags shellcheck xdg-utils
-conda activate isy
-conda install pylint rope
-conda install -c conda-forge black
-code --install-extension davidanson.vscode-markdownlint
-code --install-extension daylerees.rainglow
-code --install-extension donjayamanne.githistory
-code --install-extension dunstontc.viml
-code --install-extension formulahendry.code-runner
-code --install-extension james-yu.latex-workshop
-code --install-extension ms-python.python
-code --install-extension pkief.material-icon-theme
-code --install-extension pnp.polacode
-code --install-extension streetsidesoftware.code-spell-checker
-code --install-extension timonwong.shellcheck
-code --install-extension visualstudioexptteam.vscodeintellicode
-code --install-extension vscodevim.vim
-code --install-extension yzhang.markdown-all-in-one
-
 # Projects
 mkdir ~/Projects && cd ~/Projects || exit
 git clone https://github.com/astier/arch-installer
@@ -56,3 +35,15 @@ git clone https://github.com/astier/dotfiles
 git clone https://github.com/astier/scripts
 git clone https://github.com/astier/templates
 cd dotfiles && sh install.sh
+
+# Neovim
+yay -S nerd-fonts-dejavu-complete
+conda create -n nvi python
+conda activate nvi
+conda install -c conda-forge black pynvim
+conda update --all
+conda activate isy
+conda install isort jedi pylint rope
+pip install python-language-server
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+nvim -c PlugInstall
