4074551393c560ee40eeb235dd4b47068d5b5285
astier <aleksandrs.stier@uni-bielefeld.de>
2019-02-19 12:49:34 +0100

    Remove noto-fonts-cjk

--- arch_desktop.sh
+++ arch_desktop.sh
@@ -15,7 +15,7 @@ sudo pacman -S reflector
 sudo reflector -p https -f32 -l16 --score 8 --sort rate --save /etc/pacman.d/mirrorlist
 
 # Packages
-sudo pacman -S arc-gtk-theme biber cinnamon eog evince firefox git gnome-screenshot gnome-terminal htop libreoffice-fresh nemo-fileroller neovim noto-fonts-cjk numlockx shellcheck texlive-bibtexextra texlive-core ttf-dejavu ufw xorg-server xorg-xinit # nvidia
+sudo pacman -S arc-gtk-theme biber cinnamon eog evince firefox git gnome-screenshot gnome-terminal htop libreoffice-fresh nemo-fileroller neovim numlockx shellcheck texlive-bibtexextra texlive-core ttf-dejavu ufw xorg-server xorg-xinit # nvidia
 sudo systemctl enable NetworkManager.service systemd-timesyncd.service ufw.service
 pacman -Rns nano netctl vi
 
