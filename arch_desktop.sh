b656250de5c634654cf918c7fdcb695bfb900028
astier <aleksandrs.stier@uni-bielefeld.de>
2019-02-24 17:19:26 +0100

    Tweak

--- arch_desktop.sh
+++ arch_desktop.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# Set x- and tty-keymap
-# localectl --no-convert set-x11-keymap de pc105
-
 # Autologin
 sudo systemctl edit getty@tty1.service
 # [Service]
@@ -16,27 +13,30 @@ sudo reflector -p https -f32 -l16 --score 8 --sort rate --save /etc/pacman.d/mir
 
 # Packages
 sudo pacman -S arc-gtk-theme biber compton feh firefox git htop libreoffice-fresh light neovim networkmanager noto-fonts-cjk numlockx pulsemixer scrot shellcheck texlive-bibtexextra texlive-core ttf-dejavu ufw xorg-server xorg-xsetroot xorg-xinit xsel zathura-pdf-poppler # nvidia
+sudo pacman -Rns dhcpcd nano netctl vi
+
+# Misc
 sudo systemctl stop dhcpcd.service
 sudo systemctl enable --now NetworkManager.service systemd-timesyncd.service ufw.service
-sudo pacman -Rns dhcpcd nano netctl vi
+sudo localectl --no-convert set-x11-keymap de pc105
 
 # AUR
 git clone https://aur.archlinux.org/yay
 cd yay && makepkg -Ccirs && cd .. && rm -rf yay
 yay -S dropbox flat-remix-git i3lock-blur nerd-fonts-ubuntu-mono xbanish
 
+# Dotfiles
+mkdir Projects/ && cd Projects/ || exit
+git clone https://github.com/astier/dotfiles
+sh dotfiles/install.sh
+. dotfiles/dotfiles/.bashrc
+
 # Miniconda
 curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 sh Miniconda3-latest-Linux-x86_64.sh
 rm Miniconda3-latest-Linux-x86_64.sh .bashrc-miniconda3.bak
-. .bashrc && conda update --all
-conda create -n isy jupyter keras matplotlib pandas scikit-learn
-
-# Dotfiles
-mkdir ~/Projects && cd ~/Projects || exit
-git clone https://github.com/astier/dotfiles
-sh dotfiles/install.sh
-. ~/.bashrc
+conda update --all
+# conda create -n isy jupyter keras matplotlib pandas scikit-learn
 
 # Suckless
 git clone https://github.com/astier/dmenu
@@ -49,12 +49,12 @@ cd ../st && sudo make install clean
 # Neovim
 conda create -n nvi psutil
 conda activate nvi
-conda install -c conda-forge black pynvim
-pip install neovim-remote
+pip install neovim-remote pynvim
+conda install -c conda-forge black
 conda update --all
-conda activate isy
-conda install isort jedi pylint rope
-pip install python-language-server
+# conda activate isy
+# conda install isort jedi pylint rope
+# pip install python-language-server
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim -c PlugInstall
 
