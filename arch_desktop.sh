5b136d72affce8db68674f52cd49604f0e98ffe7
aleks <aleks.stier@icloud.com>
2019-04-08 01:10:05 +0200

    Refactor

--- arch_desktop.sh
+++ arch_desktop.sh
@@ -1,53 +1,26 @@
 #!/usr/bin/env bash
 
-# Autologin
-systemctl edit getty@tty1.service
-# [Service]
-# ExecStart=
-# ExecStart=-/usr/bin/agetty -a aleks -J %I $TERM
-
 # Mirrorlist
 systemctl start dhcpcd.service
 sudo pacman -S reflector
 sudo reflector -p https -f32 -l16 --score 8 --sort rate --save /etc/pacman.d/mirrorlist
 
 # Packages
-sudo pacman -S arc-gtk-theme biber compton feh firefox git iwd light neovim noto-fonts-cjk numlockx pulsemixer scrot texlive-bibtexextra texlive-core ttf-dejavu ufw xdg-utils xorg-server xorg-xinit xorg-xsetroot xsel zathura-pdf-poppler
-sudo pacman -Rns dhcpcd nano netctl s-nail vi
-
-# Network
-systemctl enable systemd-ead.service systemd-iwd.service systemd-networkd.service systemd-resolved.service ufw.service
-sudo ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf
-sudo ufw enable
+sudo pacman -S arc-gtk-theme biber compton feh firefox git iwd light neovim noto-fonts-cjk pulsemixer scrot texlive-bibtexextra texlive-core ttf-dejavu ufw xdg-utils xorg-server xorg-xinit xorg-xsetroot xsel zathura-pdf-poppler
+sudo pacman -Rns dhcpcd nano netctl vi
 
 # AUR
 git clone https://aur.archlinux.org/yay
 cd yay && makepkg -Ccirs
-cd .. && rm -rf yay
+cd .. && rm -fr yay
 yay -S dropbox flat-remix-git i3lock-blur nerd-fonts-ubuntu-mono xbanish
-
-# Dotfiles
-mkdir ~/Projects/
-cd ~/Projects/
-git clone https://github.com/astier/dotfiles
-sh dotfiles/install.sh
-. dotfiles/dotfiles/.bashrc
 
 # Miniconda
 curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 sh Miniconda3-latest-Linux-x86_64.sh
-rm Miniconda3-latest-Linux-x86_64.sh .bashrc-miniconda3.bak
+rm Miniconda3-latest-Linux-x86_64.sh .bashrc-miniconda.bak
 conda update --all
 # conda create -n isy jupyter keras matplotlib pandas scikit-learn
-
-# Suckless
-git clone https://github.com/astier/dmenu
-git clone https://github.com/astier/dwm
-git clone https://github.com/astier/st
-cd dmenu && sudo make install clean
-cd ../dwm && sudo make install clean
-cd ../st && sudo make install clean
-sudo ln /usr/local/bin/st /usr/bin/xterm
 
 # Neovim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -59,12 +32,33 @@ pip install neovim-remote # black
 # conda install isort jedi pylint rope
 # pip install python-language-server
 
-# Projects
+# Dotfiles & Projects
+mkdir ~/Projects/
 cd ~/Projects/
 git clone https://github.com/astier/arch
+git clone https://github.com/astier/dotfiles
 git clone https://github.com/astier/scripts
+sh dotfiles/install.sh
 sh scripts/install.sh
 
-# Misc
-systemctl enable systemd-timesyncd.service
-sudo localectl set-x11-keymap de pc105 nodeadkeys caps:swapescape
+# Suckless
+git clone https://github.com/astier/dmenu
+git clone https://github.com/astier/dwm
+git clone https://github.com/astier/st
+cd dmenu && sudo make install clean
+cd ../dwm && sudo make install clean
+cd ../st && sudo make install clean
+sudo ln /usr/local/bin/st /usr/bin/xterm
+
+# Network
+sudo systemctl enable ead.service iwd.service systemd-networkd.service systemd-resolved.service systemd-timesyncd.service ufw.service
+sudo ln -fs /run/systemd/resolve/resolv.conf /etc/resolv.conf
+sudo ufw enable
+
+# Autologin
+sudo systemctl edit getty@tty1.service
+# [Service]
+# ExecStart=
+# ExecStart=-/usr/bin/agetty -a aleks -J %I $TERM
+
+# TODO fix keymaps in xinitrc
