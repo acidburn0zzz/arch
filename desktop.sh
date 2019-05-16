f3cc9db2c2fa482170d453b1a1906880dfd0ef70
aleks <aleks.stier@icloud.com>
2019-05-16 14:30:57 +0200

    Refactor misc-section to config-section

--- desktop.sh
+++ desktop.sh
@@ -28,6 +28,13 @@ curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 sh Miniconda3-latest-Linux-x86_64.sh
 rm Miniconda3-latest-Linux-x86_64.sh
 
+# Config
+curl -fLo .local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+sudo localectl set-x11-keymap de pc105 nodeadkeys caps:swapescape
+sudo systemctl enable systemd-timesyncd.service ufw.service
+sudo ufw enable
+systemctl --user enable dropbox.service
+
 # Projects
 mkdir Projects/
 cd Projects/ || exit 1
@@ -41,12 +48,5 @@ cd ../scripts && sh install.sh
 cd ../dwm && sudo make install clean
 cd ../st && sudo make install clean
 sudo ln /usr/local/bin/st /usr/bin/xterm
-
-# Misc
-curl -fLo .local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-sudo localectl set-x11-keymap de pc105 nodeadkeys caps:swapescape
-sudo systemctl enable systemd-timesyncd.service ufw.service
-sudo ufw enable
-systemctl --user enable dropbox.service
 
 sudo reboot
