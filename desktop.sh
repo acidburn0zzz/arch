ae33e71a0856023b903ff19a3346e01fdc3b9f04
aleks <aleks.stier@icloud.com>
2019-06-16 22:41:40 +0200

    Refactor desktop sections

--- desktop.sh
+++ desktop.sh
@@ -7,24 +7,36 @@ sudo systemctl edit getty@tty1.service
 # ExecStart=-/usr/bin/agetty -a username -J %I $TERM
 
 # Internet
-# If ethernet is not available establish connection via iwd
-sudo systemctl enable --now ead.service iwd.service
-sudo systemctl start dhcpcd.service
+sudo systemctl enable --now ead.service iwd.service systemd-networkd.service systemd-resolved.service
+sudo ln -fs /run/systemd/resolve/resolv.conf /etc/resolv.conf
 
 # Packages
 sudo pacman -S reflector
 sudo reflector -p https -f32 -l16 --score 8 --sort rate --save /etc/pacman.d/mirrorlist
-sudo pacman -S arc-gtk-theme biber dash firefox fzf git hsetroot light neovim noto-fonts-cjk perl-authen-sasl pulsemixer scrot slock texlive-bibtexextra tmux ttf-dejavu ufw unclutter xorg-server xorg-xinit xsel # nvidia
-sudo pacman -Rns nano netctl s-nail vi
+sudo pacman -S arc-gtk-theme biber ctags dash firefox fzf git hsetroot light neovim noto-fonts-cjk perl-authen-sasl pulsemixer scrot shellcheck shfmt slock texlive-bibtexextra tmux ttf-dejavu ufw unclutter xorg-server xorg-xinit xsel yarn # nvidia
+sudo pacman -Rns dhcpcd nano netctl vi
 
 # AUR
 git clone https://aur.archlinux.org/yay
 cd yay && makepkg -is
-cd && rm -fr yay
-yay -S dropbox
+cd .. && rm -fr yay
+yay -S dropbox neovim-remote
+systemctl --user enable dropbox.service
+
+# Miniconda
+curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+sh Miniconda3-latest-Linux-x86_64.sh
+rm Miniconda3-latest-Linux-x86_64.sh
+
+# Configuration
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+sudo ln -sfT dash /usr/bin/sh
+sudo localectl set-x11-keymap us pc105 altgr-intl caps:swapescape
+sudo systemctl enable systemd-timesyncd.service ufw.service
+sudo enable ufw
 
 # Projects
-mkdir Projects .config
+mkdir Projects
 cd Projects/ || exit 1
 git clone https://github.com/astier/arch
 git clone https://github.com/astier/dotfiles
@@ -33,30 +45,5 @@ git clone https://github.com/astier/suckless
 cd dotfiles && sh setup.sh
 cd ../scripts && sh setup.sh
 cd ../suckless && sh setup.sh
-
-# Neovim
-sudo pacman -S ctags shellcheck shfmt yarn
-yay -S neovim-remote
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# Miniconda
-curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-sh Miniconda3-latest-Linux-x86_64.sh
-rm Miniconda3-latest-Linux-x86_64.sh
-
-# Internet
-sudo systemctl stop dhcpcd.service
-sudo pacman -Rns dhcpcd
-sudo systemctl enable systemd-networkd.service systemd-resolved.service ufw.service
-sudo cp /etc/resolv.conf /etc/resolv.conf.bak
-sudo ln -fs /run/systemd/resolve/resolv.conf /etc/resolv.conf
-sudo ufw enable
-
-# Misc
-sudo ln -sfT dash /usr/bin/sh
-sudo ln /usr/local/bin/st /usr/bin/xterm
-sudo localectl set-x11-keymap us pc105 altgr-intl caps:swapescape
-sudo systemctl enable systemd-timesyncd.service
-systemctl --user enable dropbox.service
 
 sudo reboot
