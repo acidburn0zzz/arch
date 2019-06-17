fef470a1f49a8a3485da8999c6cac98f6ad94ecc
aleks <aleks.stier@icloud.com>
2019-06-17 15:25:34 +0200

    Split suckless from projects section

--- desktop.sh
+++ desktop.sh
@@ -29,22 +29,29 @@ curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 sh Miniconda3-latest-Linux-x86_64.sh
 rm Miniconda3-latest-Linux-x86_64.sh
 
+# Projects
+mkdir Projects && cd Projects || exit 1
+git clone https://github.com/astier/arch
+git clone https://github.com/astier/dotfiles
+git clone https://github.com/astier/scripts
+cd dotfiles && sh setup.sh
+cd ../scripts && sh setup.sh
+
+# Suckless
+cd ~/Projects && mkdir suckless && cd suckless || exit
+git clone https://github.com/astier/dmenu
+git clone https://github.com/astier/dwm
+git clone https://github.com/astier/st
+cd dmenu && sudo make install clean
+cd ../dwm && sudo make install clean
+cd ../st && sudo make install clean
+sudo ln /usr/local/bin/st /usr/bin/xterm
+
 # Configuration
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 sudo ln -sfT dash /usr/bin/sh
 sudo localectl set-x11-keymap us pc105 altgr-intl caps:swapescape
 sudo systemctl enable systemd-timesyncd.service ufw.service
 sudo enable ufw
-
-# Projects
-mkdir Projects
-cd Projects/ || exit 1
-git clone https://github.com/astier/arch
-git clone https://github.com/astier/dotfiles
-git clone https://github.com/astier/scripts
-git clone https://github.com/astier/suckless
-cd dotfiles && sh setup.sh
-cd ../scripts && sh setup.sh
-cd ../suckless && sh setup.sh
 
 sudo reboot
