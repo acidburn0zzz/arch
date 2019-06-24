1cf49bd44d198561d4f4f256be21837dd3303aa4
aleks <aleks.stier@icloud.com>
2019-06-25 00:55:21 +0200

    Fix locale.gen echo bug

--- base.sh
+++ base.sh
@@ -39,7 +39,7 @@ vi /etc/hosts
 
 # Language
 echo LANG=en_US.UTF-8 > /etc/locale.conf
-echo en_US.UTF-8 UTF-8 > /etc/locale.gen
+echo en_US.UTF-8 UTF-8 >> /etc/locale.gen
 locale-gen
 
 # Bootloader
