90b1ede7aff23ce90362d3f3f2c1dfcca1f4b7c0
aleks <aleks.stier@icloud.com>
2019-06-26 15:05:52 +0200

    Revert "Switch from dwm to cinnamon"
    This reverts commit 4727d6adf2e3618c021067486a4f268a07d76899.

--- README.md
+++ README.md
@@ -4,11 +4,13 @@ My installation-scripts for *Arch Linux*.
 Currently those scripts are just templates for manual installation and are not intended to run on their own.
 
 - **OS:** [Arch Linux](https://www.archlinux.org/)
-- **DE:** Cinnamon
-- **Bootloader:** systemd-boot
-- **Network-Management**: systemd-networkd, systemd-resolved, iwd
+- **DE:** [suckless-tools](https://suckless.org/philosophy/)
 - **Encryption:** LUKS encryption via dm-encrypt/cryptsetup.
 An USB-stick with the key needs to be plugged in during bootup in order to decrypt the hard-drive.
+- **Bootloader:** systemd-boot
+- **Network-Management**: systemd-networkd, systemd-resolved, iwd
+- **Power-Management**: systemd-logind, DPMS
+- **Time-Syncronization**: systemd-timesyncd
 
 **WARNING: Always make a backup of the key-file and have at least a second USB-Key just in case you lose or break your USB-Key!
 Otherwise, you will lock yourself out of your computer.
--- desktop.sh
+++ desktop.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env sh
 
-# Internet
-# Setup networkd first (see arch-wiki)
-sudo systemctl enable --now ead.service iwd.service systemd-networkd.service systemd-resolved.service
-sudo ln -fs /run/systemd/resolve/resolv.conf /etc/resolv.conf
-
 # Autologin
 sudo systemctl edit getty@tty1.service
 # [Service]
 # ExecStart=
 # ExecStart=-/usr/bin/agetty -a username -J %I $TERM
 
+# Internet
+# Setup networkd first (see arch-wiki)
+sudo systemctl enable --now ead.service iwd.service systemd-networkd.service systemd-resolved.service
+sudo ln -fs /run/systemd/resolve/resolv.conf /etc/resolv.conf
+
 # Packages
 sudo pacman -S reflector
 sudo reflector -p https -f32 -l16 --score 8 --sort rate --save /etc/pacman.d/mirrorlist
-sudo pacman -S arc-gtk-theme biber cinnamon ctags dash firefox fzf git nemo-fileroller neovim noto-fonts-cjk scrot shellcheck shfmt texlive-bibtexextra tmux ttf-dejavu ufw unclutter xorg-server xorg-xinit xsel yarn # nvidia
+sudo pacman -S arc-gtk-theme biber ctags dash dunst firefox fzf git hsetroot light neovim newsboat noto-fonts-cjk pulsemixer scrot slock shellcheck texlive-bibtexextra tmux ttf-dejavu ufw unclutter xcompmgr xorg-server xorg-xinit xorg-xsetroot xsel yarn # nvidia
 sudo pacman -Rns dhcpcd nano netctl s-nail vi
 
 # AUR
@@ -33,9 +33,17 @@ mkdir Projects && cd Projects || exit 1
 git clone https://github.com/astier/arch
 git clone https://github.com/astier/dotfiles
 git clone https://github.com/astier/scripts
-git clone https://github.com/astier/st
 cd dotfiles && sh setup.sh
 cd ../scripts && sh setup.sh
+
+# Suckless
+cd ~/Projects && mkdir suckless && cd suckless || exit
+git clone https://git.suckless.org/sites
+git clone https://github.com/astier/dmenu
+git clone https://github.com/astier/dwm
+git clone https://github.com/astier/st
+cd dmenu && sudo make install clean
+cd ../dwm && sudo make install clean
 cd ../st && sudo make install clean
 
 # Configuration
