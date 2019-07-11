b6a6f44c047aeb42d2ad83f7f82f192ddf598394
aleks <aleks.stier@icloud.com>
2019-07-11 19:01:40 +0200

    Add tmux-plugin-manager

--- desktop.sh
+++ desktop.sh
@@ -36,6 +36,7 @@ cd ../st && sudo make install clean
 
 # CONFIGURATION
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 sudo ln -sfT dash /usr/bin/sh
 sudo localectl set-x11-keymap us pc105 altgr-intl caps:swapescape
 sudo systemctl enable systemd-timesyncd.service
