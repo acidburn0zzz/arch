c42b864d10e62ca47b7a8a3fd123b7e461c77426
aleks <aleks.stier@icloud.com>
2019-08-02 17:54:12 +0200

    Use reflector instead of utils

--- desktop.sh
+++ desktop.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
 # INSTALL
-utils -u
+sudo pacman -S reflector
+sudo reflector -p https -f16 -l8 --score 4 --sort rate --save /etc/pacman.d/mirrorlist
 sudo pacman -S arc-gtk-theme cinnamon dash fakeroot firefox gnome-terminal neovim python-neovim tmux ttf-dejavu xorg-server xorg-xinit xsel yarn
 
 # AUR
@@ -20,7 +21,6 @@ sudo systemctl enable NetworkManager.service fstrim.timer systemd-timesyncd.serv
 
 # CLEAN
 sudo pacman -Rns diffutils dhcpcd gettext iproute2 iputils jfsutils licenses logrotate lvm2 man-pages mdadm nano netctl psmisc reiserfsprogs s-nail usbutils vi which xfsprogs
-utils -c
 rm ~/.bash_logout ~/.cache/go-build
 
 sudo reboot
