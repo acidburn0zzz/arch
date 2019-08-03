757a7ef0c8105107f299382896c1724f9ac7991f
aleks <aleks.stier@icloud.com>
2019-08-03 23:27:49 +0200

    Add gcc, make, termite; remove go

--- desktop.sh
+++ desktop.sh
@@ -3,7 +3,7 @@
 # INSTALL
 sudo pacman -S reflector
 sudo reflector -p https -f16 -l8 --score 4 --sort rate --save /etc/pacman.d/mirrorlist
-sudo pacman -S arc-gtk-theme cinnamon dash fakeroot firefox gnome-terminal neovim python-neovim tmux ttf-dejavu xorg-server xorg-xinit xsel yarn
+sudo pacman -S arc-gtk-theme cinnamon dash fakeroot firefox gcc make neovim python-neovim termite tmux ttf-dejavu xorg-server xorg-xinit xsel yarn
 
 # AUR
 git clone https://aur.archlinux.org/yay
@@ -20,7 +20,7 @@ sudo localectl set-x11-keymap us pc105 altgr-intl caps:swapescape
 sudo systemctl enable NetworkManager.service fstrim.timer systemd-timesyncd.service
 
 # CLEAN
-sudo pacman -Rns diffutils dhcpcd gettext iproute2 iputils jfsutils licenses logrotate lvm2 man-pages mdadm nano netctl psmisc reiserfsprogs s-nail usbutils vi which xfsprogs
+sudo pacman -Rns diffutils dhcpcd gettext go iproute2 iputils jfsutils licenses logrotate lvm2 man-pages mdadm nano netctl psmisc reiserfsprogs s-nail usbutils vi which xfsprogs
 sudo pacman -Sc
 rm ~/.bash_logout ~/.cache/go-build
 
