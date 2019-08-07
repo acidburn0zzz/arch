cee9f13ca0cb6ed943bc7d2e5d987aceea76e1cd
aleks <aleks.stier@icloud.com>
2019-08-07 12:00:43 +0200

    Replace termite with st, create projects-section

--- desktop.sh
+++ desktop.sh
@@ -3,7 +3,7 @@
 # INSTALL
 sudo pacman -S reflector
 sudo reflector -p https -f16 -l8 --score 4 --sort rate --save /etc/pacman.d/mirrorlist
-sudo pacman -S arc-gtk-theme cinnamon dash fakeroot firefox fzf gcc make neovim python-neovim termite tmux ttf-dejavu xorg-server xorg-xinit xsel yarn
+sudo pacman -S arc-gtk-theme cinnamon dash fakeroot firefox fzf gcc make neovim pkgconf python-neovim tmux ttf-dejavu xorg-server xorg-xinit xsel yarn
 
 # AUR
 git clone https://aur.archlinux.org/yay
@@ -11,10 +11,14 @@ cd yay && makepkg -is
 cd .. && rm -fr yay
 yay -S dropbox flat-remix
 
+# PROJECTS
+cd ~/projects || exit
+git clone https://github.com/astier/st
+cd dotfiles && sh setup.sh
+cd ../st && sudo make install clean
+
 # CONFIGURATION
-cd ~/projects/dotfiles && sh setup.sh
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-sudo ln /usr/bin/termite /usr/bin/xterm
 sudo ln -sfT dash /usr/bin/sh
 sudo localectl set-x11-keymap us pc105 altgr-intl caps:swapescape
 sudo systemctl enable NetworkManager.service fstrim.timer systemd-timesyncd.service
