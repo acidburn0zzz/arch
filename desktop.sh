7b33ec605827dee68af873e23cb59ec6807c3f35
aleks <aleks.stier@icloud.com>
2019-08-09 16:28:12 +0200

    Remove feh and xcompmgr

--- desktop.sh
+++ desktop.sh
@@ -8,7 +8,7 @@ sudo ln -fs /run/systemd/resolve/resolv.conf /etc/resolv.conf
 # INSTALL
 sudo pacman -S reflector
 sudo reflector -p https -f16 -l8 --score 4 --sort rate --save /etc/pacman.d/mirrorlist
-sudo pacman -S arc-gtk-theme dash fakeroot feh firefox gcc git herbstluftwm light make neovim noto-fonts-cjk pkgconf pulsemixer python-neovim sxkhd tmux ttf-dejavu xcompmgr xorg-server xorg-xinit xsel yarn
+sudo pacman -S arc-gtk-theme dash fakeroot firefox gcc git herbstluftwm light make neovim noto-fonts-cjk pkgconf pulsemixer python-neovim sxkhd tmux ttf-dejavu xorg-server xorg-xinit xsel yarn
 
 # AUR
 git clone https://aur.archlinux.org/yay
