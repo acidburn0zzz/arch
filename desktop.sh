3ec08eeefa40dddc5bef6dfbb88497f8ac9a9575
aleks <aleks.stier@icloud.com>
2019-08-13 18:58:10 +0200

    Replace dwm and dmenu with hlwm and rofi

--- desktop.sh
+++ desktop.sh
@@ -6,23 +6,18 @@ sudo systemctl enable --now ead.service iwd.service systemd-networkd.service sys
 sudo ln -fs /run/systemd/resolve/resolv.conf /etc/resolv.conf
 
 # PACKAGES
-sudo pacman -S arc-gtk-theme dash fakeroot firefox gcc git light make neovim noto-fonts-cjk pulsemixer python-neovim sx tmux ttf-dejavu xorg-server xsel yarn
+sudo pacman -S arc-gtk-theme dash fakeroot firefox gcc git herbstluftwm light make neovim noto-fonts-cjk pulsemixer python-neovim rofi sx tmux ttf-dejavu xorg-server xsel yarn
 git clone https://aur.archlinux.org/yay
 cd yay && makepkg -is
 yay -S dropbox
 
-# SUCKLESS
+# PROJECTS
 cd ~/projects || exit
-git clone https://git.suckless.org/sites
-git clone https://github.com/astier/dmenu
-git clone https://github.com/astier/dwm
 git clone https://github.com/astier/st
-cd dmenu && sudo make install clean
-cd ../dwm && sudo make install clean
-cd ../st && sudo make install clean
+cd st && sudo make install clean
+cd ../dotfiles && sh setup.sh
 
 # CONFIG
-cd ../dotfiles && sh setup.sh
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 chsh -s /bin/dash
 sudo ln -sfT dash /usr/bin/sh
