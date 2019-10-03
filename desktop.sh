4ead28b69d5d49c1f51c7f72acfb00ad8ac8b1a1
aleks <aleks.stier@icloud.com>
2019-10-03 23:42:52 +0200

    Remove blank line

--- desktop.sh
+++ desktop.sh
@@ -5,7 +5,6 @@ sudo cp -fr ~/projects/dotfiles/dotfiles/systemd/network /etc/systemd
 sudo systemctl enable --now ead.service iwd.service systemd-networkd.service systemd-resolved.service
 sudo ln -fs /run/systemd/resolve/resolv.conf /etc/resolv.conf
 
-
 # PACKAGES
 sudo pacman -S arc-gtk-theme dash fakeroot firefox gcc herbstluftwm light make neovim pulsemixer python-neovim sx sxhkd tmux ttf-dejavu xorg-server xsel yarn
 
