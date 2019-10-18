89c4a56a6e749e33417b6c2e16d4088385662585
aleks <aleks.stier@icloud.com>
2019-10-18 14:50:17 +0200

    Do not configre x11 keymap

--- desktop.sh
+++ desktop.sh
@@ -25,7 +25,6 @@ cd ../dotfiles && sh setup.sh
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 chsh -s /bin/dash
 sudo ln -sfT dash /usr/bin/sh
-sudo localectl set-x11-keymap us pc105 altgr-intl caps:swapescape
 sudo systemctl enable fstrim.timer iptables.service systemd-timesyncd.service
 
 # CLEAN
