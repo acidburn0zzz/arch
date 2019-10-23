eaad3d5f07bbf9c8299a559943dd69319c4ea1fd
aleks <aleks.stier@icloud.com>
2019-10-23 14:50:41 +0200

    Move dotfiles to config-section

--- desktop.sh
+++ desktop.sh
@@ -19,9 +19,9 @@ git clone https://github.com/astier/st
 cd dmenu && sudo make install clean
 cd ../slock && sudo make install clean
 cd ../st && sudo make install clean
-cd ../dotfiles && sh setup.sh
 
 # CONFIGURE
+cd ~/projects/dotfiles && sh setup.sh
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 chsh -s /bin/dash
 sudo ln -sfT dash /usr/bin/sh
