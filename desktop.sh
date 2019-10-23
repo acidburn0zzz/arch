b884a632e4bf31741a2b6dc340eb4b0f53dd3105
aleks <aleks.stier@icloud.com>
2019-10-23 20:54:14 +0200

    Replace neovim-remote with python-pynvim

--- desktop.sh
+++ desktop.sh
@@ -6,10 +6,10 @@ sudo systemctl enable --now ead.service iwd.service systemd-networkd.service sys
 sudo ln -fs /run/systemd/resolve/resolv.conf /etc/resolv.conf
 
 # PACKAGES
-sudo pacman -S arc-gtk-theme dash fakeroot firefox fzf gcc herbstluftwm light make neovim noto-fonts-cjk pulsemixer sx sxhkd tmux ttf-dejavu xorg-server xsel yarn
+sudo pacman -S arc-gtk-theme dash fakeroot firefox fzf gcc herbstluftwm light make neovim noto-fonts-cjk pulsemixer python-pynvim sx sxhkd tmux ttf-dejavu xorg-server xsel yarn
 cd && git clone https://aur.archlinux.org/yay
 cd yay && makepkg -is
-yay -S dropbox neovim-remote
+yay -S dropbox
 
 # PROJECTS
 cd ~/projects || exit
