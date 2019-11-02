779d2d5a1df88c304299be9071aa2771be0376c7
aleks <aleks.stier@icloud.com>
2019-11-02 22:48:23 +0100

    Remove redundant cd

--- desktop.sh
+++ desktop.sh
@@ -7,7 +7,7 @@ sudo ln -fs /run/systemd/resolve/resolv.conf /etc/resolv.conf
 
 # PACKAGES
 sudo pacman -S arc-gtk-theme dash fakeroot firefox fzf gcc herbstluftwm light make man-db noto-fonts-cjk pulsemixer python-pynvim sx sxhkd tmux ttf-dejavu xorg-server xsel yarn
-cd && git clone https://aur.archlinux.org/yay
+git clone https://aur.archlinux.org/yay
 cd yay && makepkg -is
 yay -S dropbox
 
@@ -18,11 +18,11 @@ git clone https://github.com/astier/dmenu
 git clone https://github.com/astier/slock
 git clone https://github.com/astier/st
 cd dmenu && sudo make install clean
+cd ../dotfiles && sh setup.sh
 cd ../slock && sudo make install clean
 cd ../st && sudo make install clean
 
 # CONFIGURE
-cd ~/projects/dotfiles && sh setup.sh
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 chsh -s /bin/dash
 sudo ln -sfT dash /usr/bin/sh
